3fbdf63599c997ab142eeb26da3d24858c2717e0
Keyu Zhang <keyuz4@uci.edu>
2021-01-24 02:16:57 -0800

    add separate class for analytics

---
+++ project2/frontier_state/analytics.py
@@ -0,0 +1,15 @@
+
+
+
+# 1. Keep track of the subdomains that it visited, and count how many different URLs it has
+# processed from each of those subdomains.
+# 2. Find the page with the most valid out links (of all pages given to your crawler). Out Links are the
+# number of links that are present on a particular webpage.
+# 3. List of downloaded URLs and identified traps.
+# 4. What is the longest page in terms of number of words? (HTML markup doesn’t count as words)
+# 5. What are the 50 most common words in the entire set of pages? (Ignore English stop words,
+# which can be found, (https://www.ranks.nl/stopwords)
+
+class Analytics:
+    def __init__(self):
+        pass
