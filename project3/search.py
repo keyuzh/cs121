fe51a3624339febffe1456093693a814173af1b0
cyruslo5028 <57110218+cyruslo5028@users.noreply.github.com>
2021-02-28 19:30:16 -0800

    updated cosine similarity

--- project3/search.py
+++ project3/search.py
@@ -15,6 +15,8 @@
 
 import pickle
 import sys
+import math
+import itertools
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow,QLineEdit,QPushButton,QLabel
 from PyQt5.QtGui import QPixmap
@@ -23,15 +25,17 @@ from PyQt5.QtGui import QPixmap
 
 
 class Search:
-    def __init__(self, index:dict, se):
+    def __init__(self, index:dict, normalized_tf:dict, se):
         self.index = index
+        self.normalized_tf = normalized_tf
         self.SearchEngine = se
         self.window = None
         self.logo = None
         self.textbox = None
         self.button = None
 
-    def search_url(self, keyword):
+    #search return at most K urls where K = 20 in this case
+    def search_url(self, keyword,K):
         '''out_list = []
         if keyword in self.index.keys():
             for l in self.index[keyword]:
@@ -39,18 +43,58 @@ class Search:
 
         out_list.sort(key=(lambda x: -x[1]))
         return out_list'''
-        word_frequency = self.word_fq(keyword)
-        try:
-            inverted_index = pickle.load(open("inverted_index","rb"))
-            normalized_tf = pickle.load(open("normalized_tf","wb"))
-        except:
-            print("Failed to load file")
+        word_frequency = self.word_fq(keyword) # Get frequency of each word
+        print("After tokenize query:")
+        for word,fq in word_frequency.items():
+            print (word,fq)
+        word_frequency = self.normalize_tfidf(word_frequency) # get normalized value of each word
+        print("After norm query;")
+        for word,fq in word_frequency.items():
+            print (word,fq)        
+        doc_score = self.calculate_cosine_similarity(word_frequency) # return dict of doc with their score as value
+        print("After cosine similarity: ")
+        for doc, score in doc_score.items():
+            print (doc,score)
+        sorted(doc_score.items(),key=lambda x:x[1])
+        return dict(itertools.islice(doc_score.items(),K))
+
+    # Return a dict() with
+    # token = DocID, value = score
+    def calculate_cosine_similarity(self,word_frequency):
+        doc_score = dict()
+        for qword,qnorm in word_frequency.items():
+            for did, doc in self.normalized_tf.items():
+                if qword in doc.keys(): # word in the document
+                    doc_score[did] += qnorm * doc[qword]
+        return doc_score
+
         
 
+    #Calculate tfidf and normalize
+    def normalize_tfidf(self, word_frequency):
+        #Courpus Count
+        total_documents = len(self.normalized_tf)
+        query_length = 0
+        for word, freq in word_frequency.items():
+            #if word in index -> calculate tfidf
+            if word in self.index.keys():
+                print("In index")
+                df = len(index[word])
+                word_frequency[word] = 1+math.log(freq,10) * math.log(total_documents/df,10)
+                query_length += word_frequency[word]**2
+            # else tfidf = 0
+            else:
+                print("Not in Index")
+                word_frequency[word] = 0
+        query_length = math.sqrt(query_length)
+        for word, freq in word_frequency.items():
+            if freq != 0:
+                freq = freq / query_length
+        return word_frequency
         
     #find word frequency in query
     def word_fq (self, word):
-        query_token = self.keyword.split()
+        query_token = word.split()
         query_dict = dict()
         for w in query_token:
             if w not in query_dict:
@@ -64,10 +108,13 @@ class Search:
             return len(self.index[keyword])
 
     def search_button_clicked(self, textbox):
-        print("button clicked")
+        print("Seaching...")
         print(self.textbox.text())
         #return a list of urls
-        urls = self.search_url()
+        urls = self.search_url(self.textbox.text(),20)
+        print("Finished.\nResult:")
+        for doc, score in urls:
+            print(doc,score)
         """
         window = QMainWindow()
         window.setGeometry(200,200,1000,1000)
@@ -120,7 +167,8 @@ if __name__ == '__main__':
     """
     se = QApplication(sys.argv)
     inverted_index = pickle.load(open("inverted_index","rb"))
-    search = Search(inverted_index, se)
+    normalized_tf = pickle.load(open("normalized_tf","rb"))
+    search = Search(inverted_index,normalized_tf, se)
     search.home_page()
 
 
