4397e1d5147af684a80f69d585a523908ed5bd68
keyuzh <keyuz4@uci.edu>
2021-03-03 18:04:45 -0800

    rename main.py to build.py

--- project3/build.py
+++ project3/build.py
@@ -2,14 +2,34 @@ from corpus import Corpus
 from lemmatization import Tokenize
 from index import Index
 
-import sys
+import getopt, sys
 import atexit
 
 DEBUG = False
 
 if __name__ == '__main__':
-    path = sys.argv[1]
-    corpus = Corpus(path)
+    # https://www.geeksforgeeks.org/command-line-arguments-in-python/
+    # Remove 1st argument from the
+    # list of command line arguments
+    argumentList = sys.argv[1:]
+    # Options
+    options = "bc:"
+    # Long options
+    long_options = ["corpus =", "bigram"]
+    try:
+        # Parsing argument
+        arguments, values = getopt.getopt(argumentList, options, long_options)
+        print(arguments, values)
+        # checking each argument
+        for currentArgument, currentValue in arguments:
+            print(currentArgument, currentValue)
+    except getopt.error as err:
+        # output error, and return with an error code
+        print(str(err))
+
+
+    corpus_path = sys.argv[1]
+    corpus = Corpus(corpus_path)
     token = Tokenize()
     index = Index(corpus.get_bookkeeping())
     if not DEBUG:
--- project3/corpus.py
+++ project3/corpus.py
@@ -5,37 +5,39 @@ from pathlib import Path
 import lxml.html
 
 
+def json_to_dict(json_path: Path or str) -> dict:
+    """load json to a dict"""
+    with open(json_path, encoding='utf8') as json_file:
+        return json.load(json_file)
+
+
 class Corpus:
     def __init__(self, corpus_path: str or Path):
-        if isinstance(corpus_path, str):
-            corpus_path = Path(corpus_path)
-        self.path = corpus_path
-        self.bookkeeping = self._json_to_dict(self.path / "bookkeeping.json")
-
-    def _json_to_dict(self, json_path: Path) -> dict:
-        with open(json_path, encoding='utf8') as json_file:
-            return json.load(json_file)
-
+        self.path = Path(corpus_path)
+        # dict to convert path to url
+        self.bookkeeping = json_to_dict(self.path / "bookkeeping.json")
 
     def feed_html(self) -> dict:
-        """iterator that yields html from local corpus"""
+        """generator that yields html from local corpus"""
         for path, url in self.bookkeeping.items():
             html_content = self.get_html(path)
             yield (html_content, url, path)
 
     def get_html(self, path: str) -> str:
+        """retrieve the webpage html from corpus and return the content as string"""
         with self.get_file(path) as f:
             return f.read()
 
     def get_file(self, path) -> open:
-        path_list = path.split('/')
-        directory = path_list[0]
-        file = path_list[1]
-        file_path = self.path / directory / file
+        """retrieve the webpage as an open file"""
+        # path_list = path.split('/')
+        # directory = path_list[0]
+        # file = path_list[1]
+        file_path = self.path / path
         return open(file_path, encoding='utf8')
 
-
     def get_bookkeeping(self):
+        """returns the bookkeeping dict"""
         return self.bookkeeping
 
     def get_url(self, path: str) -> str:
@@ -46,6 +48,6 @@ class Corpus:
         #https://stackoverflow.com/questions/51233/how-can-i-retrieve-the-page-title-of-a-webpage-using-python
         t = lxml.html.parse(self.get_file(path))
         try:
-            return t.find(".//title").text
+            return lxml.html.parse(self.get_file(path)).find(".//title").text
         except:
             return "(Description not found)"
